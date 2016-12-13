80d9a352ad3e2ff21d731cda3928a5fa5eacead2
alexpoe22 <alexpoe22@gmail.com>
2016-12-12 17:36:15 -0800

    actions handle main functionality of hot and cold game

--- js/index.js
+++ js/index.js
@@ -1,21 +1,15 @@
 require('babel-polyfill');
-
 import * as actions from './actions/index'
-
 import store from './store'
 
-console.log(store);
-
-//learn about this
 // subscribe is a method that listens for changes on the store
 let unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 }) 
 
-
+	store.dispatch(actions.addGuess(10))
+	store.dispatch(actions.addGuess(13))
+	store.dispatch(actions.addGuess(11))
 	store.dispatch(actions.newGame())
-	store.dispatch(actions.addGuess(45))
-	store.dispatch(actions.addGuess(3))
-
-
+	
 unsubscribe();
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -3,17 +3,51 @@ import * as actions from '../actions/index';
 const emptyState = {
 	guessedNumbers: [],
 	answer: 0,
-	feedback: 'make a guess'
+	feedback: 'make a guess',
+	count: 0
 }
 
 export const mainReducer = (state = emptyState, action) => {
 	if (action.type === actions.ADD_GUESS) {
+		var response = '';
+		var ln = state.answer;
+		var un = action.num;
+		var diff = Math.abs(ln - un)
+		var count = state.count;
+
+	    if (diff === 0) {
+			response += 'you are freaking incredible, wow good job, good effort'
+
+		} else if (diff < 10) { 
+   			response += 'you are so hot, i can\'t even compute'
+			
+		} else if (diff < 20 && diff > 10) {
+			response += 'its okay... some of us are just not meant to win'
+	
+		} else if (diff < 40 && diff > 20) {
+			response += 'maybe you should just give up'
 		
-		return { ...state, guessedNumbers: [...state.guessedNumbers, action.num], feedback: "try again"}
+		} else {
+			response += 'i honestly dont know what to tell ya.. '
+		}
+		count++
+		
+		return { ...state, 
+			guessedNumbers: [...state.guessedNumbers, action.num], 
+			feedback: response,
+			count: count
+			}
 	}
+	
 	if(action.type === actions.NEW_GAME) {
-		return { ...state, answer: action.magicNum }
+		return { ...state,
+		 answer: action.magicNum,
+		 count: 0,
+		 feedback: 'welcome, prepare to be destroyed',
+		 guessedNumbers: []
+		 }
 	}
+
 	return state;
 };
 
