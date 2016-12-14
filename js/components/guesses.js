0d1cb5cf69764a825704e798080f1dd6840b1547
numbergames <bfletch@gmail.com>
2016-12-13 19:33:16 -0500

    tweaks and reformatting

--- js/components/guesses.js
+++ js/components/guesses.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const Guesses = props => (
-	<ul className="guessbox">
-		{props.text.map((n, i) =>
-			<li key={i}>
-        {n}
-      </li>
-		)}
-	</ul>
-);
+const Guesses = props => {
+  const heading = (props.text.length)
+    ? 'Your guesses so far:'
+    : '';
 
+  return (
+    <div className="guessbox">
+      <h3 className="guessbox-heading">
+        {heading}
+      </h3>
+      <ul className="guesses">
+    		{props.text.map((n, i) =>
+    			<li key={i}>
+            {n}
+          </li>
+    		)}
+    	</ul>
+    </div>
+  );
+}
 export default Guesses;
